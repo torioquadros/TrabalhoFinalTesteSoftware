625c699319cb05eba750c97bb532729b983450c0
Vitório Quadros da Silva <torioquadros@gmail.com>
2026-06-16 22:03:59 -0300

    adicionado mais alguns testes

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,9 +1,13 @@
-import { test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { test, expect, afterEach } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/vitest";
 
 import App from "./App";
+
+afterEach(() => {
+  cleanup();
+});
 
 test("ao clicar em Criar exibe a tela de criação", async () => {
   render(<App />);
@@ -15,3 +19,17 @@ test("ao clicar em Criar exibe a tela de criação", async () => {
   expect(
     screen.getByText("Criar Usuário")).toBeInTheDocument();
 });
+
+test("permite digitar um nome", async () => {
+  render(<App />);
+
+  await userEvent.click(screen.getByText("Criar"));
+
+  const input = screen.getByPlaceholderText("Digite o nome");
+
+  await userEvent.type(input, "Vitorio");
+
+  expect(input).toHaveValue("Vitorio");
+});
+
+
